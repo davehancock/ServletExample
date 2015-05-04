bb12df0cca622423e111243b08615ab7fea3b8ef
daves125125 <daves125125@gmail.com>
2015-05-04 13:39:05 +0100

    initial commit

---
+++ Servlet2StaticContent/src/main/java/com/djh/example/ExampleServlet.java
@@ -0,0 +1,27 @@
+package com.djh.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author David Hancock
+ */
+public class ExampleServlet extends HttpServlet {
+
+    public static final String FOO = "foobar";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println(FOO);
+    }
+
+}
---
+++ SpringBootStaticContent/src/main/java/com.djh.example/StaticContentLoader.java
@@ -0,0 +1,16 @@
+package com.djh.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author David Hancock
+ */
+@SpringBootApplication
+public class StaticContentLoader {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticContentLoader.class, args);
+    }
+
+}
